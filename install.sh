cf12e6e906ff4587d87ecc195ed176937b535774
Marcus Mo <marcusmo88@gmail.com>
2018-05-19 11:12:05 +0100

    Fixed shellcheck warnings - mainly use of double quotes

--- install.sh
+++ install.sh
@@ -29,17 +29,17 @@ sudo apt update && sudo apt install -y \
 sudo pip install flake8
 
 echo "Moving dotfiles files into position..."
-rsync -av ./dotfiles/ $HOME/
+rsync -av ./dotfiles/ "$HOME/"
 
 echo "Moving config files into position..."
-mkdir -p $HOME/.config
-rsync -avr ./config/ $HOME/.config/
+mkdir -p "$HOME/.config"
+rsync -avr ./config/ "$HOME/.config/"
 
-mkdir -p $HOME/.local
-rsync -avr ./local/ $HOME/.local/
+mkdir -p "$HOME/.local"
+rsync -avr ./local/ "$HOME/.local/"
 
 echo "Resizing and configuring wallpapers..."
-DIMENSIONS=$(xrandr | grep '*' | awk '{ print $1 }')
+DIMENSIONS=$(xrandr | grep '[*]' | awk '{ print $1 }')
 DESKTOP_SOURCE="wallpapers/mikael_gustafsson_small_memory.png"
 DESKTOP_TARGET="wallpapers/desktop.png"
 LOCKSCREEN_SOURCE="wallpapers/ill_leave_tomorrows_problems_to_tomorrows_me.jpg"
@@ -51,24 +51,24 @@ convert "$LOCKSCREEN_SOURCE" -resize "$DIMENSIONS^" -gravity center -extent "$DI
 echo "Installing vim plugins..."
 AUTOLOAD_DIR=$HOME/.vim/autoload
 BUNDLE_DIR=$HOME/.vim/bundle
-mkdir -p $AUTOLOAD_DIR $BUNDLE_DIR
+mkdir -p "$AUTOLOAD_DIR" "$BUNDLE_DIR"
 
-[ ! -f "$AUTOLOAD_DIR/pathogen.vim" ] && curl -LSso $AUTOLOAD_DIR/pathogen.vim https://tpo.pe/pathogen.vim
+[ ! -f "$AUTOLOAD_DIR/pathogen.vim" ] && curl -LSso "$AUTOLOAD_DIR/pathogen.vim" https://tpo.pe/pathogen.vim
 
-[ ! -d "$BUNDLE_DIR/goyo.vim"      ] && git clone https://github.com/junegunn/goyo.vim.git $BUNDLE_DIR/goyo.vim
-[ ! -d "$BUNDLE_DIR/nerdcommenter" ] && git clone https://github.com/scrooloose/nerdcommenter.git $BUNDLE_DIR/nerdcommenter
-[ ! -d "$BUNDLE_DIR/nerdtree"      ] && git clone https://github.com/scrooloose/nerdtree.git $BUNDLE_DIR/nerdtree
-[ ! -d "$BUNDLE_DIR/vim-flake8"    ] && git clone https://github.com/nvie/vim-flake8.git $BUNDLE_DIR/vim-flake8
-[ ! -d "$BUNDLE_DIR/vim-fugitive"  ] && git clone https://github.com/tpope/vim-fugitive.git $BUNDLE_DIR/vim-fugitive && \
+[ ! -d "$BUNDLE_DIR/goyo.vim"      ] && git clone https://github.com/junegunn/goyo.vim.git "$BUNDLE_DIR/goyo.vim"
+[ ! -d "$BUNDLE_DIR/nerdcommenter" ] && git clone https://github.com/scrooloose/nerdcommenter.git "$BUNDLE_DIR/nerdcommenter"
+[ ! -d "$BUNDLE_DIR/nerdtree"      ] && git clone https://github.com/scrooloose/nerdtree.git "$BUNDLE_DIR/nerdtree"
+[ ! -d "$BUNDLE_DIR/vim-flake8"    ] && git clone https://github.com/nvie/vim-flake8.git "$BUNDLE_DIR/vim-flake8"
+[ ! -d "$BUNDLE_DIR/vim-fugitive"  ] && git clone https://github.com/tpope/vim-fugitive.git "$BUNDLE_DIR/vim-fugitive" && \
     vim -u NONE -c "helptags vim-fugitive/doc" -c q
 
-[ ! -d "$BUNDLE_DIR/vim-colors-solarized" ] && git clone https://github.com/altercation/vim-colors-solarized.git $BUNDLE_DIR/vim-colors-solarized
+[ ! -d "$BUNDLE_DIR/vim-colors-solarized" ] && git clone https://github.com/altercation/vim-colors-solarized.git "$BUNDLE_DIR/vim-colors-solarized"
 
-[ ! -d "$BUNDLE_DIR/YouCompleteMe" ] && git clone https://github.com/Valloric/YouCompleteMe.git $BUNDLE_DIR/YouCompleteMe && \
-    cd $BUNDLE_DIR/YouCompleteMe && \
+[ ! -d "$BUNDLE_DIR/YouCompleteMe" ] && git clone https://github.com/Valloric/YouCompleteMe.git "$BUNDLE_DIR/YouCompleteMe" && \
+    cd "$BUNDLE_DIR/YouCompleteMe" && \
     git submodule update --init --recursive && \
     ./install.py --clang-completer && \
-    cd -
+    cd - || exit
 
 echo "Installing fzf..."
 command -v fzf >/dev/null 2>&1 || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
