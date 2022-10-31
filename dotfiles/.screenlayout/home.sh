67ea0137658c7c84b65ed96fded9673343cf6a24
Marcus Mo <marcus.mo@fluidly.com>
2022-10-31 10:01:10 +0000

    Add screen layouts

---
+++ dotfiles/.screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 328x0 --rotate normal --scale 1.3334x1.3334 --output eDP1 --primary --mode 2560x1440 --pos 328x1440 --rotate normal --output DP2 --off
---
+++ dotfiles/.screenlayout/home_3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2560x1440 --pos 440x1440 --rotate normal --output DP1 --mode 1920x1080 --pos 3440x0 --rotate left --scale 1.3334x1.3334 --output DP2 --mode 3440x1440 --pos 0x0 --rotate normal --output VIRTUAL1 --off
